750f504e2245079e0c70f5a3fcb3bf0c257c05ee
sysamir <alekberov.samir@mail.ru>
2016-12-27 16:33:53 +0400

    movzulara bashlamaq

--- app/Http/Controllers/SubjectsController.php
+++ app/Http/Controllers/SubjectsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Subjects extends Controller
+class SubjectsController extends Controller
 {
     /**
      * Display a listing of the resource.
